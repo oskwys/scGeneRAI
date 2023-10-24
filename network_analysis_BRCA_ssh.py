63ff3649959d963716689c6044380e6ab117f198
Oskar Wysocki <39080017+oskwys@users.noreply.github.com>
2023-10-24 14:09:37 +0200

    Update network_analysis_BRCA_ssh.py

--- network_analysis_BRCA_ssh.py
+++ network_analysis_BRCA_ssh.py
@@ -29,8 +29,10 @@ import importlib, sys
 importlib.reload(f)
 # %%
 
-networks_for_patient_groups_and_ALL_genes = False
-networks_for_patient_groups_and_pathway_genes = False
+networks_for_patient_groups_and_ALL_genes = True
+networks_for_patient_groups_and_pathway_genes = True
+plot_clustermaps_pathway = True
+plot_umaps = True
 
 # %% load data
 
@@ -141,7 +143,10 @@ if networks_for_patient_groups_and_pathway_genes == True:
                     i+=1
                     
                 network_all = f.add_edge_colmn(network_all)
-                network_all = network_all.groupby(by = ['edge','source_gene', 'target_gene','edge_type'],as_index=False).mean()
+                
+                network_all = network_all.groupby(by = ['edge','source_gene', 'target_gene','edge_type'],as_index=False).agg(mean='mean', median='median', std='std', q1=lambda x: x.quantile(0.25),  q3=lambda x: x.quantile(0.75)).reset_index()
+                
+                #network_all = network_all.groupby(by = ['edge','source_gene', 'target_gene','edge_type'],as_index=False).mean()
                 
                 network_all.to_excel(os.path.join(path_to_save, 'network_{}_{}.xlsx'.format(subgroup, pathway)))
                 
@@ -167,11 +172,19 @@ if networks_for_patient_groups_and_ALL_genes == True:
                 lrp_array[:, i] = temp['LRP'].values
                 
     
-            lrp_array_mean = np.mean(lrp_array,axis=1)
+            lrp_array_mean = np.round(  np.mean(lrp_array,axis=1), 5)
+            lrp_array_std = np.round( np.std(lrp_array,axis=1), 5)
+            lrp_array_median = np.round( np.median(lrp_array,axis=1), 5)
+            lrp_array_q1 = np.round( np.quantile(lrp_array, .25, axis=1), 5)
+            lrp_array_q3 = np.round( np.quantile(lrp_array, .25, axis=1), 5)
             
             network_all = temp.copy()
-            network_all['LRP'] = lrp_array_mean
-            network_all['LRP'] = network_all['LRP'].round(5)
+            network_all['LRP_mean'] = lrp_array_mean
+            network_all['LRP_std'] = lrp_array_mean
+            network_all['LRP_median'] = lrp_array_mean
+            network_all['LRP_q1'] = lrp_array_mean
+            network_all['LRP_q3'] = lrp_array_mean
+            
             network_all = f.add_edge_colmn(network_all)
     
             network_all.to_csv(os.path.join(path_to_save, 'network_{}_allgenes.csv'.format(subgroup)))
@@ -198,7 +211,7 @@ for pathway in genes_pathways['Pathway'].unique():
     
 lrp_dict_filtered_pd = {}
 for pathway in genes_pathways['Pathway'].unique():
-    
+    print(pathway, ' ---- get lrp_dict_filtered for each pathway PD')
     lrp_dict_filtered_pd[pathway] = f.get_lrp_dict_filtered_pd(lrp_dict_filtered, pathway = pathway)    
 
 
@@ -207,57 +220,60 @@ for pathway in genes_pathways['Pathway'].unique():
 
 thresholds = [0, 0.0001, 0.0002]
 
-for threshold in thresholds:
-    for pathway in genes_pathways['Pathway'].unique():
-        print('Clustermap for ', pathway, str(threshold))
-        data_to_clustermap = lrp_dict_filtered_pd[pathway].copy()
-        
-        data_to_clustermap[data_to_clustermap < threshold] = 0
-        
-        df_clinical_features_ = f.get_column_colors_from_clinical_df(df_clinical_features,data_to_clustermap )
-        column_colors = f.map_subtypes_to_col_color(df_clinical_features_)    
-        
-        sns.clustermap(data_to_clustermap, mask  = data_to_clustermap < threshold, cmap = 'jet', col_colors = column_colors,
-                       method = 'ward', yticklabels = False, xticklabels = False, vmax = 0.008, vmin = 0)       
-        plt.suptitle(pathway, fontsize=20)       
-               
-        plt.savefig(os.path.join(path_to_save, 'clustermap_{}_th{}.png'.format(pathway, str(threshold)[2:])), dpi = 400)        
+if plot_clustermaps_pathway:
+    for threshold in thresholds:
+        for pathway in genes_pathways['Pathway'].unique():
+            print('Clustermap for ', pathway, str(threshold))
+            data_to_clustermap = lrp_dict_filtered_pd[pathway].copy()
+            
+            data_to_clustermap[data_to_clustermap < threshold] = 0
+            
+            df_clinical_features_ = f.get_column_colors_from_clinical_df(df_clinical_features,data_to_clustermap )
+            column_colors = f.map_subtypes_to_col_color(df_clinical_features_)    
+            
+            sns.clustermap(data_to_clustermap, mask  = data_to_clustermap < threshold, cmap = 'jet', col_colors = column_colors,
+                           method = 'ward', yticklabels = False, xticklabels = False, vmax = 0.008, vmin = 0)       
+            plt.suptitle(pathway, fontsize=20)       
+                   
+            plt.savefig(os.path.join(path_to_save, 'clustermap_{}_th{}.png'.format(pathway, str(threshold)[2:])), dpi = 400)        
         
 # %%% plot UMAP
 import umap
 titles = ['her2', 'estrogen_receptor','progesterone_receptor',  'TNBC']
 
-for threshold in thresholds:
-    for pathway in genes_pathways['Pathway'].unique():
-        print('UMAP for ', pathway, str(threshold))
-        data_to_umap = lrp_dict_filtered_pd[pathway].copy().T
-        
-        data_to_umap[data_to_umap < threshold] = 0
-        
-        reducer = umap.UMAP()
-        embedding = reducer.fit_transform(data_to_umap)
-        
-        df_clinical_features_ = f.get_column_colors_from_clinical_df(df_clinical_features, lrp_dict_filtered_pd[pathway].copy() )
-        column_colors = f.map_subtypes_to_col_color(df_clinical_features_)    
-         
-        
-        # Create a 2x2 grid of subplots
-        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
-        axes = axes.flatten()  # Flatten axes to easily iterate over
-        
-        # Iterate over subplots and color lists to create UMAP plots
-        i=0
-        for ax, colors in zip(axes, column_colors):
-            scatter = ax.scatter(embedding[:, 0], embedding[:, 1], c=colors.values)
-            ax.set_title(titles[i])
-            i+=1
-            ax.set_xticks([])
-            ax.set_yticks([])
-        plt.suptitle(pathway, fontsize=20)    
-        plt.tight_layout()
+
+if plot_umaps:
+    for threshold in thresholds:
+        for pathway in genes_pathways['Pathway'].unique():
+            print('UMAP for ', pathway, str(threshold))
+            data_to_umap = lrp_dict_filtered_pd[pathway].copy().T
+            
+            data_to_umap[data_to_umap < threshold] = 0
+            
+            reducer = umap.UMAP()
+            embedding = reducer.fit_transform(data_to_umap)
+            
+            df_clinical_features_ = f.get_column_colors_from_clinical_df(df_clinical_features, lrp_dict_filtered_pd[pathway].copy() )
+            column_colors = f.map_subtypes_to_col_color(df_clinical_features_)    
              
-        plt.savefig(os.path.join(path_to_save, 'UMAP_{}_th{}.png'.format(pathway, str(threshold)[2:])), dpi = 400)        
-        
+            
+            # Create a 2x2 grid of subplots
+            fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
+            axes = axes.flatten()  # Flatten axes to easily iterate over
+            
+            # Iterate over subplots and color lists to create UMAP plots
+            i=0
+            for ax, colors in zip(axes, column_colors):
+                scatter = ax.scatter(embedding[:, 0], embedding[:, 1], c=colors.values)
+                ax.set_title(titles[i])
+                i+=1
+                ax.set_xticks([])
+                ax.set_yticks([])
+            plt.suptitle(pathway, fontsize=20)    
+            plt.tight_layout()
+                 
+            plt.savefig(os.path.join(path_to_save, 'UMAP_{}_th{}.png'.format(pathway, str(threshold)[2:])), dpi = 400)        
+    
     
 
 # %%% clustermap legend
@@ -274,5 +290,84 @@ for threshold in thresholds:
 # plt.axis('off')
 # plt.show()
     
+# %% PCA and UMAP for all
+
+zz
+
+lrp_array = np.zeros((index_.shape[0], len(samples)))
+ 
+ 
+for index, (sample_name, data) in enumerate(lrp_dict.items()):
+    print(index, sample_name)
+    lrp_array[:, index]  = data['LRP'].values
+
+
+lrp_array_mean = np.mean(lrp_array, axis =1 )
+lrp_array_mean_pd = pd.DataFrame(data = np.array([lrp_array_mean, data['source_gene'], data['target_gene']]).T,  index= data.reset_index(drop=True).index, columns = ['LRP','source_gene','target_gene']   ).sort_values('LRP', ascending = False).reset_index()
     
+lrp_array_mean_pd['LRP'].plot()
+
+
+# get 10% of highest mean LRP
+threshold = 0.01 # %
+lrp_array_mean_pd_topn = lrp_array_mean_pd.iloc[:int(lrp_array_mean_pd.shape[0] * threshold/100), :]
+
+lrp_array_all_topn = lrp_array[lrp_array_mean_pd_topn['index'].values, :].T
+
+
+
+# %%% PCA
+from sklearn.decomposition import PCA
+
+# Perform PCA
+pca = PCA(n_components=3)  # Reducing to 2 components
+X_pca = pca.fit_transform(lrp_array_all_topn)
+
+# Print the explained variance ratio
+explained_variance = pca.explained_variance_ratio_
+print(f"Explained variance ratio: {explained_variance}")
+
+# Create a 2D scatter plot
+fig, ax = plt.subplots(figsize = (8,8))
+scatter = ax.scatter(X_pca[:, 0], X_pca[:, 1], cmap='viridis', s=10)
+
+ax.set_xlabel(f'Principal Component 1 ({explained_variance[0]:.2%})')
+ax.set_ylabel(f'Principal Component 2 ({explained_variance[1]:.2%})')
+ax.set_title('2D PCA of Iris Dataset')
+
+# Display the plot
+plt.show()
+
+
+
+
+# Perform UMAP
+reducer = umap.UMAP(n_neighbors=15, n_components=2, min_dist=0.1, metric='euclidean')
+X_umap = reducer.fit_transform(lrp_array_all_topn)
+
+# Plot
+fig, ax = plt.subplots(figsize = (8,8))
+scatter = ax.scatter(X_umap[:, 0], X_umap[:, 1], cmap='viridis', s=10)
+
+
+ax.set_xlabel('UMAP 1')
+ax.set_ylabel('UMAP 2')
+ax.set_title('2D UMAP of Iris Dataset')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
          

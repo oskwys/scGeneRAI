b3d30c12e143644e7d55881fe8d5200d1d3873de
Oskar Wysocki <39080017+oskwys@users.noreply.github.com>
2023-10-26 21:39:25 +0200

    Update clustering_01_ssh.py

--- clustering_01_ssh.py
+++ clustering_01_ssh.py
@@ -29,6 +29,15 @@ from datetime import datetime
 import importlib, sys
 importlib.reload(f)
 
+# %%
+
+calculate_UMAP = False#True
+
+calculate_PCA = True
+
+calculate_linkage = True
+
+
 # %% load data
 
 path_to_data = '/home/d07321ow/scratch/scGeneRAI/data/data_BRCA'
@@ -44,7 +53,7 @@ path_to_lrp_results = '/home/d07321ow/scratch/results_LRP_BRCA/results'
 data_to_model, df_exp, df_mut, df_amp, df_del, df_fus, df_clinical_features = f.get_input_data(path_to_data)
 
 # %% get samples
-samples = f.get_samples_with_lrp(path_to_lrp_results)
+samples = f.get_samples_with_lrp(path_to_lrp_results)[:30]
 pd.DataFrame(samples, columns = ['samples']).to_csv(os.path.join(path_to_save, 'BRCA_samples'))
 
 print('Samples: ', len(samples))
@@ -66,7 +75,7 @@ for file in os.listdir(path_to_lrp_results):
         lrp_files.append(file)
         
         
-n = len(lrp_files)  
+n =30# len(lrp_files)  
 
 #network_data = pd.DataFrame()
 start_time = datetime.now()
@@ -122,32 +131,79 @@ lrp_array_diff_pd['LRP_variability'].plot()
 
 
 # get 10% of highest mean LRP
-thresholds = [0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001, 0.0005] # %
-n_neighbors_list = [5, 10, 15, 20, 25, 30]
-min_dist_list = [0.01, 0.05, 0.1, 0.15, 0.2, 0.5]
+thresholds = [0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001] # %
 
-for threshold in thresholds:
-    for n_neighbors in n_neighbors_list:
-        for min_dist in min_dist_list:
-            print('Threshold: ', threshold, n_neighbors, min_dist)
+
+# %% UMAP
+
+if calculate_UMAP:
+    n_neighbors_list = [5, 10, 15, 20, 25, 30]
+    min_dist_list = [0.01, 0.05, 0.1, 0.15, 0.2, 0.5]
+    
+    for threshold in thresholds:
+        for n_neighbors in n_neighbors_list:
+            for min_dist in min_dist_list:
+                print('Threshold: ', threshold, n_neighbors, min_dist)
+                
+                lrp_array_pd_topn = lrp_array_diff_pd.iloc[:int(lrp_array_diff_pd.shape[0] * threshold/100), :]
             
-            lrp_array_pd_topn = lrp_array_diff_pd.iloc[:int(lrp_array_diff_pd.shape[0] * threshold/100), :]
+                lrp_array_pd_topn = lrp_array[lrp_array_pd_topn['index'].values, :].T
+            
+                
+                import umap
+                # Perform UMAP
+                reducer = umap.UMAP(n_neighbors=n_neighbors, n_components=2, min_dist = min_dist, metric='euclidean')
+                X_umap = reducer.fit_transform(lrp_array_pd_topn)
+                
+                X_umap = pd.DataFrame(X_umap, columns = ['umap_1', 'umap_2'])
+                X_umap.to_csv(os.path.join(path_to_save, 'umap_thres{}_n{}_dist{}.csv'.format(str(threshold).replace('.',''), n_neighbors, str(min_dist).replace('.',''))))
+                
+                
+# %% PCA
+from sklearn.decomposition import PCA
+if calculate_PCA:
+    components = 9
+    explained_variances = []
+    for threshold in thresholds:
+
+        print('PCA Threshold: ', threshold)
+        lrp_array_pd_topn = lrp_array_diff_pd.iloc[:int(lrp_array_diff_pd.shape[0] * threshold/100), :]
+        lrp_array_pd_topn = lrp_array[lrp_array_pd_topn['index'].values, :].T
+    
+        pca = PCA(n_components=components)  # Reducing to 2 components
+        X_pca = pca.fit_transform(lrp_array_pd_topn)
         
-            lrp_array_pd_topn = lrp_array[lrp_array_pd_topn['index'].values, :].T
-        
-            
-            import umap
-            # Perform UMAP
-            reducer = umap.UMAP(n_neighbors=n_neighbors, n_components=2, min_dist = min_dist, metric='euclidean')
-            X_umap = reducer.fit_transform(lrp_array_pd_topn)
-            
-            X_umap = pd.DataFrame(X_umap, columns = ['umap_1', 'umap_2'])
-            X_umap.to_csv(os.path.join(path_to_save, 'umap_thres{}_n{}_dist{}.csv'.format(str(threshold).replace('.',''), n_neighbors, str(min_dist).replace('.',''))))
-            
-            
-            
+        # Print the explained variance ratio
+        explained_variance = pca.explained_variance_ratio_
+                
+        X_pca = pd.DataFrame(X_pca, columns = ['PCA_' + str(i+1) for i in range(components)])
+        X_pca.to_csv(os.path.join(path_to_save, 'PCA_thres{}.csv'.format(str(threshold).replace('.',''))))
+        explained_variances.append(list(explained_variance))
+    
+    explained_variances_df = pd.DataFrame(explained_variances)#, columns = ['threshold',''])
+    explained_variances_df['thresholds'] = thresholds
+    explained_variances_df.to_csv(os.path.join(path_to_save, 'PCA_explained_variance.csv'))
     
     
+# %% AGGLOMERATIVE CLUSTERING
+from scipy.cluster.hierarchy import dendrogram, linkage
+   
+if calculate_linkage:
+    for threshold in thresholds:
+
+        print('linkage Threshold: ', threshold)
+        lrp_array_pd_topn = lrp_array_diff_pd.iloc[:int(lrp_array_diff_pd.shape[0] * threshold/100), :]
+        lrp_array_pd_topn = lrp_array[lrp_array_pd_topn['index'].values, :].T
+        
+        Z_single = linkage(lrp_array_pd_topn, 'single')
+        Z_average = linkage(lrp_array_pd_topn, 'average')
+        Z_complete = linkage(lrp_array_pd_topn, 'complete')
+        Z_ward = linkage(lrp_array_pd_topn, 'ward')
+    
+        pd.DataFrame(Z_single).to_csv(os.path.join(path_to_save, 'Z_single.csv'))
+        pd.DataFrame(Z_average).to_csv(os.path.join(path_to_save, 'Z_average.csv'))
+        pd.DataFrame(Z_complete).to_csv(os.path.join(path_to_save, 'Z_complete.csv'))
+        pd.DataFrame(Z_ward).to_csv(os.path.join(path_to_save, 'Z_ward.csv'))
     
     
 #     # Plot
@@ -164,14 +220,10 @@ for threshold in thresholds:
     
 
 # # %%% PCA
-# from sklearn.decomposition import PCA
+# 
 
 # # Perform PCA
-# pca = PCA(n_components=3)  # Reducing to 2 components
-# X_pca = pca.fit_transform(lrp_array_pd_topn)
-
-# # Print the explained variance ratio
-# explained_variance = pca.explained_variance_ratio_
+# 
 # print(f"Explained variance ratio: {explained_variance}")
 
 # # Create a 2D scatter plot
